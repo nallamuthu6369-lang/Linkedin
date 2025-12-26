261c9a7ea238b682406c6b7bf77ca11931d5f195
nallamuthu <nallamuthu6369@gmail.com>
2025-12-26 16:39:19 +0530

    updating LI001

--- src/main/java/com/LinkedIn/App.java
+++ src/main/java/com/LinkedIn/App.java
@@ -5,6 +5,7 @@ package com.LinkedIn;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("in LI001");
+        System.out.println("----------");
     }
 }
